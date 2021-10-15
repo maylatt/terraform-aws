4d3a63e7112211ccdfa3ca8719b99c1c25e03f4c
Mayla Teixeira <maylateixeira2013@gmail.com>
2021-10-15 19:13:45 -0300

    Fixes Output Values

--- infrastructure/aws/output.tf
+++ infrastructure/aws/output.tf
@@ -1,9 +1,9 @@
 output "bucket_names" {
-    value = var.bucket_names
+    value = local.bucket_names
 }
 
-output "bucket_paths" {
-  value = local.bucket_paths
+output "bucket_functions" {
+  value = local.bucket_functions
 }
 
 output "database_names" {
