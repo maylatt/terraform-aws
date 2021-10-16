0a3f7698958d04f693249b0b10cb4180d1244286
Mayla Teixeira <maylateixeira2013@gmail.com>
2021-10-15 23:57:49 -0300

    Fixes Local Reference

--- infrastructure/aws/backend.tf
+++ infrastructure/aws/backend.tf
@@ -1,7 +1,7 @@
 # Backend configuration require a AWS storage bucket.
 terraform {
   backend "s3" {
-    bucket = locals.backend_bucket
+    bucket = local.backend_bucket
     key    = "state/terraform.tfstate"
     region = "us-east-1"
   }
