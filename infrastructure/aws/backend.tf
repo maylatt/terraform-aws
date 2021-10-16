234cbbfc3a419d274403682bc5cd214228199537
Mayla Teixeira <maylateixeira2013@gmail.com>
2021-10-16 00:03:13 -0300

    Removes Variable Not Allowed in Backend Config

--- infrastructure/aws/backend.tf
+++ infrastructure/aws/backend.tf
@@ -1,7 +1,7 @@
 # Backend configuration require a AWS storage bucket.
 terraform {
   backend "s3" {
-    bucket = local.backend_bucket
+    bucket = "a3-terraform-backend-827486940322"
     key    = "state/terraform.tfstate"
     region = "us-east-1"
   }
