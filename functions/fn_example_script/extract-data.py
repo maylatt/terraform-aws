e182b72486358fb7f938db4023f8eefa991f9ad9
Mayla Teixeira <maylateixeira2013@gmail.com>
2021-10-18 12:26:15 -0300

    Implements Lambda Function

---
+++ functions/fn_example_script/extract-data.py
@@ -0,0 +1,27 @@
+import boto3
+import urllib.request
+
+# Constantes
+tmp_folder_path = "/tmp"
+bucket_folder = "raw-data"
+
+def get_data(file_url, file_name):
+    urllib.request.urlretrieve(file_url, f'{tmp_folder_path}/{file_name}')
+    return True
+
+def handler(event, context):
+
+    bucket_name = event['BUCKET']
+    file_url = event['FILE_URL']
+
+    file_name = file_url.split('/')[-1]
+    res = get_data(file_url, file_name)
+
+    s3 = boto3.client("s3")
+    object_name = f'{bucket_folder}/{file_name}'
+    s3.upload_file(file_name, bucket_name, object_name)
+
+    return {
+        'statusCode': 200,
+        'body': f"Subiu o arquivo {object_name} para o bucket {bucket_name}. E nois que voa!"
+    }
