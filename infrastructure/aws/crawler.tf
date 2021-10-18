5d645936698146ecda2857e579a74f736368ded4
Mayla Teixeira <maylateixeira2013@gmail.com>
2021-10-18 14:11:56 -0300

    Adds Glue Database Resource

--- infrastructure/aws/crawler.tf
+++ infrastructure/aws/crawler.tf
@@ -1,7 +1,11 @@
+resource "aws_glue_catalog_database" "consumer" {
+  name = var.database_name
+}
+
 resource "aws_glue_crawler" "consumer" {
   name          = "${local.prefix}-${var.database_name}_crawler"
   role          = aws_iam_role.glue_role.arn
-  database_name = var.database_name
+  database_name = aws_glue_catalog_database.consumer.name
   tags          = local.common_tags
 
   s3_target {
